fb441e01045f9b6d7bee213760dfc13c8dd47248
danielhe27 <jdah49d@gmail.com>
2024-01-15 18:49:37 -0500

    buttons functionality and timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,3 +17,37 @@ var restartButton = document.getElementById("restart-btn");
 var scoreField = document.getElementById("player-score");
 var scores = JSON.parse(localStorage.getItem("scores")) || [];
 
+// this variable is for the questions on my quiz
+var shuffledQuestions, currentQuestionIndex;
+
+// is a listener wich mean, the code needs to do something qhen i click on start game
+startButton.addEventListener("click", startGame);
+
+// this listener sets up next question
+nextButton.addEventListener("click", () => {
+    currentQuestionIndex++
+    setNextQuestion()
+});
+
+// this timer functions make a countdown if the timer drops to 0 needs to call to save score function 
+function timeTick() {
+  timeLeft--;
+  timerEl.textContent = "Time: " + timeLeft;
+  if (timeLeft <= 0) {
+      saveScore();
+  }
+}
+
+// Function to start the game, how fast the timer goes in the count, also set a random question every time the game start
+function startGame() {
+  timerID = setInterval(timeTick, 1000);
+  startContainerEl.classList.add("hide");
+  shuffledQuestions = questions.sort(() => Math.random() - .5)
+  currentQuestionIndex = 0
+  questionContainerEl.classList.remove("hide");
+
+  // timer will start when the button is clicked 
+  timeTick();
+  setNextQuestion();
+};
+
